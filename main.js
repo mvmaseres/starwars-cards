ccdb053e56da99a596c5b030002594f6dba47989
Valentina Maseres <mvmaseres@gmail.com>
2024-01-05 19:05:50 +0100

    recuperar todos los datos de api

--- main.js
+++ main.js
@@ -6,13 +6,15 @@ const speciesUrl = 'species/'
 const starshipsUrl = 'starships/'
 const vechiclesUrl = 'vehicles/'
 
-async function fetchAPI() {
-
-    const responseFilms = await fetch(apiUrl + filmsUrl)
-    const data = await responseFilms.json()
-    const films = data.results
-    console.log(films)
-
+async function fetchApi(apiType) {
+    const response = await fetch('https://swapi.dev/api/' + apiType)
+    const data = await response.json()
+    return data.results
 }
 
-fetchAPI()
+const films = await fetchApi(filmsUrl)
+const people = await fetchApi(peopleUrl)
+const planets = await fetchApi(planetsUrl)
+const species = await fetchApi(speciesUrl)
+const starships = await fetchApi(starshipsUrl)
+const vehicles = await fetchApi(vechiclesUrl)
