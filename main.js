3dccb528ccecb2cd59492bb85ad661e3924442f4
Valentina Maseres <mvmaseres@gmail.com>
2024-01-05 18:27:12 +0100

    get data films from API

--- main.js
+++ main.js
@@ -5,3 +5,14 @@ const planetsUrl = 'planets/'
 const speciesUrl = 'species/'
 const starshipsUrl = 'starships/'
 const vechiclesUrl = 'vehicles/'
+
+async function fetchAPI() {
+
+    const responseFilms = await fetch(apiUrl + filmsUrl)
+    const data = await responseFilms.json()
+    const films = data.results
+    console.log(films)
+
+}
+
+fetchAPI()
