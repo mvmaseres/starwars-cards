9bb1f8cecfa9047630cf238cc9d11a80e7d9a49d
Valentina Maseres <mvmaseres@gmail.com>
2024-01-07 00:30:55 +0100

    add some assets

---
+++ scripts/templates.js
@@ -0,0 +1,19 @@
+async function addHeaderAndFooter() {
+    const response = await fetch('templates.html')
+
+    if (!response.ok) {
+        console.log('Error fetching header and footer')
+        return
+    }
+
+    const templates = document.createElement('template')
+    templates.innerHTML = await response.text()
+
+    const headerTemplate = templates.content.querySelector('#template-header').content
+    document.querySelector('#header').appendChild(headerTemplate)
+
+    const footerTemplate = templates.content.querySelector('#template-footer').content
+    document.querySelector('#footer').appendChild(footerTemplate)
+}
+
+addHeaderAndFooter()
