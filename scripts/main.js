6cffba2e34afcd8303d05da03783c7b2479d561f
Valentina Maseres <mvmaseres@gmail.com>
2024-01-16 16:19:58 +0100

    add js to login sign up html to show password

--- scripts/main.js
+++ scripts/main.js
@@ -63,6 +63,26 @@ liDifficulty.forEach(item => {
 })
 
 /* LOG IN / SIGN UP */
+const forms = document.querySelectorAll('.forms')
+const pwShowHide = document.querySelectorAll('.eye-icon')
+
+pwShowHide.forEach(eyeIcon => {
+    eyeIcon.addEventListener("click", () => {
+        let pwFields = eyeIcon.parentElement.parentElement.querySelectorAll(".password")
+
+        pwFields.forEach(password => {
+            if(password.type === "password") {
+                password.type = "text"
+                eyeIcon.classList.replace("bx-hidde", "bx-show")
+                return
+            }
+            password.type = "password"
+            eyeIcon.classList.replace("bx-show", "bx-hidde")
+        })
+    })
+})
+
+
 
 
 /* Bring the API */
