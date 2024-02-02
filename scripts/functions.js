bc9a6195307364f06a98199b5695df20693e898f
Valentina Maseres <mvmaseres@gmail.com>
2024-02-02 23:23:40 +0100

    add an spinner that represent that when you introduce the correct username and password, the page is loading to do other action

--- scripts/functions.js
+++ scripts/functions.js
@@ -11,17 +11,29 @@ export function authenticate() {
         const wrongMessage = document.getElementById('wrong-container')
         const loginDiv = document.getElementById('login-page')
         const main = document.querySelector('main')
+        const loadingSpinner = document.getElementById('loading-spinner');
 
-        // Verifica el usuario y la contraseña (para demostración, solo un usuario válido)
+        //Check the user and password. Just a one valid option
         if (username === 'demo1' && password === 'demo100') {
-            messageContainer.classList.remove('hide')
+            
+            //remove the login form
             loginDiv.classList.add('hide')
+
+            //aply some styles
             main.style.display = 'flex'
             main.style.justifyContent = 'center'
             main.style.marginTop = '2rem'
             wrongMessage.classList.add('hide')
 
+            //show the message to redirect and spinner to redirect to other page
+            messageContainer.classList.remove('hide')
+            loadingSpinner.classList.remove('hide');
+            setTimeout(() => {
+                window.location.href = 'game.html';
+            }, 3000);
+
         } else {
+            //show a message to wrong user o password
             wrongMessage.classList.remove('hide')
         }
 })
